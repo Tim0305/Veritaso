ce8934c3caa27478bf5c1cde7f0b32970b837bde
Tim0305 <tabtim1015@gmail.com>
2025-03-30 00:41:51 -0600

    FIX

--- src/veritaso_frontend/src/App.jsx
+++ src/veritaso_frontend/src/App.jsx
@@ -1,17 +1,29 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import Articulo from "./pages/Articulo/Articulo";
 import Buscador from "./pages/Buscador/Buscador";
-import Inicio from "./pages/Inicio/Inicio";
+import NavBar from "./components/NavBar/NavBar";
+import SearchResults from "./pages/SearchResults/SearchResults";
+import Articulo from "./pages/Articulo/Articulo";
+import NotFound from "./pages/NotFound/NotFound";
+import ModificarArticulo from "./pages/ModificarArticulo/ModificarArticulo";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Buscador />} />
-        <Route path="/inicio" element={<Inicio />} />
-      </Routes>
-    </Router>
+    <>
+      <NavBar />
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Buscador />} />
+            <Route path="/resultados" element={<SearchResults />} />
+            <Route path="/articulo" element={<Articulo />} />
+            <Route path="/modificarArticulo" element={<ModificarArticulo />} />
+            {/* Redirige a Home si la ruta no existe */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
